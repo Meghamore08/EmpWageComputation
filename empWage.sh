f46b048a98b6ae0e27b3ace06c40826065a83d3b
Megha <meghamoree@gmail.com>
2021-04-22 23:32:53 +0530

    Added: Calculate Wage for a month

---
+++ empWage.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -x
+
+empCheck=$((RANDOM%3))
+isFullTime=1
+isPartTime=2
+empRatePerHr=20
+
+case $empcheck in
+    $isFullTime)
+        empHrs=8
+      ;;
+      $isPartTime)
+        empHrs=4
+      ;;
+      *)
+        empHrs=0
+      ;;
+esac
+
+Salary=$(($empHrs*$empRatePerHr))
+echo $Salary
+
+
+
+
+
+
