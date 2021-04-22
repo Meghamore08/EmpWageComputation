4f6d7b4ea66075933d48ad2611a6d9881642eab8
Megha <meghamoree@gmail.com>
2021-04-22 23:45:47 +0530

    Added: Working Hours Calculating

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -6,12 +6,16 @@ IS_FULL_TIME=1
 IS_PART_TIME=2
 EMP_RATE_PER_HR=20
 NUM_WORKING_DAYS=20
+MAX_HRS_IN_MONTH=100
 
 #variable
-totalSalary=0
+totalworkingDay=0
+totalEmpHrs=0
 
-for (( day=1; day<=$NUM_WORKING_DAYS; day++ ))
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalworkingDay -lt $NUM_WORKING_DAYS ]]
 do
+
+        ((totalworkingDay++))
         empCheck=$((RANDOM%3))
         case $empCheck in
         $IS_FULL_TIME)
@@ -25,14 +29,9 @@ do
         ;;
         esac
 
-        salary=$(($empHrs*$EMP_RATE_PER_HR))
-        totalSalary=$(($totalSalary+$salary))
-
+        totalEmpHrs=$(($totalEmpHrs+$empHrs))
 done
-
-
-
-
+        totalSalary=$(($totalEmpHrs*+$EMP_RATE_PER_HR))
 
 
 
