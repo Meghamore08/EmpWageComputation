6dac863b2f35e024a54744abe33fa68d94fd8406
Megha <meghamoree@gmail.com>
2021-04-23 00:01:56 +0530

    Modified: To Calculate working Hours

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,33 +5,37 @@
 IS_FULL_TIME=1
 IS_PART_TIME=2
 EMP_RATE_PER_HR=20
-NUM_WORKING_DAYS=20
-MAX_HRS_IN_MONTH=100
+NUM_WORKING_DAYS=5
+MAX_HRS_IN_MONTH=20
 
 #variable
-totalworkingDay=0
+totalWorkingDay=0
 totalEmpHrs=0
 
-while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalworkingDay -lt $NUM_WORKING_DAYS ]]
+function getWorkingHrs() {
+    case $1 in
+        $IS_FULL_TIME)
+          empHrs=8
+         ;;
+         $IS_PART_TIME)
+           empHrs=4
+         ;;
+         *)
+           empHrs=0
+         ;;
+     esac
+     echo $empHrs
+}
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDay -lt $NUM_WORKING_DAYS ]]
 do
 
-        ((totalworkingDay++))
-        empCheck=$((RANDOM%3))
-        case $empCheck in
-        $IS_FULL_TIME)
-                empHrs=8
-        ;;
-        $IS_PART_TIME)
-                empHrs=4
-        ;;
-        *)
-                empHrs=0
-        ;;
-        esac
-
-        totalEmpHrs=$(($totalEmpHrs+$empHrs))
+        ((totalWorkingDay++))
+        workHrs="$( getWorkingHrs $((RANDOM%3)) )"
+        totalEmpHrs=$(($totalEmpHrs+$workHrs))
 done
-        totalSalary=$(($totalEmpHrs*+$EMP_RATE_PER_HR))
+        totalSalary=$(($totalEmpHrs*$EMP_RATE_PER_HR))
+
+
 
 
 
